cdc73aa064f1b6f80629396289ac3f43ba7acc2a
Nel Benlloch <nel.benllochg@gmail.com>
2025-05-21 10:19:55 +0100

    First commit:
    New file which will run through simulations on the RCS.Copy over the functions to be used and basic layout

---
+++ Rcode/Simulations_wrapper_PANTHER.R
@@ -0,0 +1,143 @@
+########################################################################################################
+########################################################################################################
+# 8888888 .d8888b. 88888888888 888     888                                     
+#   888  d88P  Y88b    888     888     888                                     
+#   888  888    888    888     888     888                                     
+#   888  888           888     888     888                                     
+#   888  888           888     888     888                                     
+#   888  888    888    888     888     888                                     
+#   888  Y88b  d88P    888     Y88b. .d88P                                     
+# 8888888 "Y8888P"     888      "Y88888P"                                      
+# 
+# 8888888b.     d8888 888b    888 88888888888 888    888 8888888888 8888888b.  
+# 888   Y88b   d88888 8888b   888     888     888    888 888        888   Y88b 
+# 888    888  d88P888 88888b  888     888     888    888 888        888    888 
+# 888   d88P d88P 888 888Y88b 888     888     8888888888 8888888    888   d88P 
+# 8888888P" d88P  888 888 Y88b888     888     888    888 888        8888888P"  
+# 888      d88P   888 888  Y88888     888     888    888 888        888 T88b   
+# 888     d8888888888 888   Y8888     888     888    888 888        888  T88b  
+# 888    d88P     888 888    Y888     888     888    888 8888888888 888   T88b 
+########################################################################################################
+########################################################################################################
+########################################################################################################
+
+
+# File title: Simulations_wrapper_PANTHER
+# 
+# Input: N/A
+# 
+# Output: Output a single table with informaition on eff/fut/non-conv/neither results in many sims
+# 
+# Purpose: Model PANTHER using BATTS and a lot of simulations
+
+
+# ------------------ Author ---------------- Date ---------------- Update:
+#            | Manel Benlloch Guajardo | 01MAY2025 |   Initial version of the file.
+
+########################################################################################################
+########################################################################################################
+########################################################################################################
+
+library(BATSS)
+library(tidyr)
+library(magrittr)
+library(dplyr)
+library(INLA)
+
+
+setwd(paste0(rstudioapi::getSourceEditorContext()$path,"/.."))
+
+source(paste0(getwd(),"/batss_glm_breakdown.R"))
+
+Wrapper<- function(
+        beta_list,
+        model,var,var.control=NULL,family="gaussian",link="identity",
+        which,alternative = "greater",R=1e+4,N,interim,prob0,
+        delta.eff=0,delta.fut=delta.eff, delta.RAR=0,
+        eff.arm,eff.arm.control=NULL,
+        eff.trial=NULL,eff.trial.control=NULL,
+        fut.arm,fut.arm.control=NULL,
+        fut.trial=NULL,fut.trial.control=NULL,
+        RAR=NULL,RAR.control=NULL,
+        H0=TRUE,
+        computation="parallel",
+        mc.cores=getOption("mc.cores", 3L),
+        map_probabilities =FALSE,
+        #linux.os = NA,
+        extended=0, ...){
+    
+    data <- as.data.frame(matrix(NA,m0,n.var,
+                                 dimnames=list(paste0("1-",1:m0),id.var)))
+    
+    
+    for (i in 2:length(beta_list)){
+        beta_baseline<-beta_list[1]
+        beta<-list(beta_baseline,# this is the control
+                   beta_list[i] # this is the treatment 
+                )
+        glm_pom_run<-batss.glm.pom( model,var,var.control,family,link,
+                                    beta,which,alternative ,R,N,interim,prob0,
+                                    delta.eff,delta.fut, delta.RAR,
+                                    eff.arm,eff.arm.control,
+                                    eff.trial,eff.trial.control,
+                                    fut.arm,fut.arm.control,
+                                    fut.trial,fut.trial.control,
+                                    RAR,RAR.control,
+                                    H0,computation,
+                                    mc.cores,
+                                    map_probabilities,
+                                    extended, ...)
+    }
+    scenario1$H1$target$global
+}
+
+
+
+# below what goes on a normal function
+
+batss.glm.pom(   
+    model           = y ~ treatment,
+    var             = list(y = multinomial_random,
+                           treatment = treatalloc.fun),
+    var.control     = list(y = list(size= 1)),
+    family          = "pom",
+    link            = 'identity',
+    beta            = list(multinom_rand_dset1,# this is the control
+                           multinom_rand_dset2 # this is the treatment 
+    ), 
+    which           = c(2),   # Select which groups are treatments
+    R               = Trials,
+    control.fixed = list(mean = list( treatment = 0), prec = 0.1),
+    control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE, config = TRUE),
+    alternative     = c("greater")
+    , # One or two sided hypothesis
+    map_probabilities = TRUE, # This variable will apply new maps to the days of
+    # day free support- estimation.
+    
+    # RAR:
+    # RAR option not used as we have uniform sampling 
+    #   - continuously sample to Max sample size/ Futitility/ Efficacy
+    #RAR             = prob.trippa,
+    #RAR.control     = list("gamma"=3, "eta"=1.4,"nu"=0.1),
+    #delta.RAR       = 0,
+    
+    prob0           = c("UC"=1,"Simvastatin"=1),#,"Baricitinib"=1),
+    N               = 504*2, # Assume the maximum cap of hypoinflammatory is reached
+    interim         = list(recruited=list(m0 = 89*2 #89*3 # Trigger interim at 89 patients per arm
+                                          ,m  = 49*2  # As per the recruitment expected Do interims at 49/ arm
+    )),
+    
+    eff.arm         = efficacy.arm.fun, # Efficiency function of posteriors
+    delta.eff       = log(1.1), # Select which interims select efficiency beta P(beta > delta.fut)
+    eff.arm.control = list(b.eff = 0.84), # select the probability of the posterior > beta  
+    fut.arm         = futility.arm.fun,
+    delta.fut       = log(1.075), # select the analysed efficiency beta P(beta > delta.fut)
+    fut.arm.control = list(b.fut = 1-0.78), # select the probability of the posterior > beta  
+    delta.RAR       = 0,
+    computation     = "parallel",
+    mc.cores        = parallel::detectCores()-1,
+    H0              = FALSE,
+    eff.trial=efficacy.arm.fun,
+    fut.trial=futility.arm.fun,
+    RAR = NULL,
+    extended = 2)
