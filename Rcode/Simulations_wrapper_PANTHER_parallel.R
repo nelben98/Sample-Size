b76094a2103706e0058f73c5bb792cde6a1f670e
Nel Benlloch <nel.benllochg@gmail.com>
2025-06-09 18:11:52 +0100

    Parallelized:
    Changed from paralelization to by node and then each node to only parallelize once rather than by for loop.

--- Rcode/Simulations_wrapper_PANTHER_parallel.R
+++ Rcode/Simulations_wrapper_PANTHER_parallel.R
@@ -51,7 +51,8 @@ if (length(commandArgs(trailingOnly=TRUE))!=0){
     pres_wd = as.numeric(args[2])} else {
     setwd(paste0(rstudioapi::getSourceEditorContext()$path,"/../.."))
     pres_wd = getwd()}
-if(length(commandArgs(trailingOnly=TRUE)) <= 2) {m=1 # setting the m variable 
+if(length(commandArgs(trailingOnly=TRUE)) <= 2) {
+    m=1 # setting the m variable 
 } else {m = as.numeric(args[3])} # M will be the array job split - if applicatble
 
 #nnodes = as.numeric(args[3]) # not needed unles many simulation chosen
@@ -62,6 +63,7 @@ primOutDist_panth<- read.csv(paste0(pres_wd,"/excel_distributions/VFDdistributio
 source(paste0(pres_wd,"/Rcode/batss_glm_breakdown.R"))
 
 Wrapper<- function(
+        number_node =1,
         beta_list,
         model,var,var.control=NULL,family="gaussian",link="identity",
         which,alternative = "greater",R=1e+4,N,interim,prob0,
@@ -84,26 +86,13 @@ Wrapper<- function(
                                   dimnames=list(paste0(colnames(beta_list[,-1])),
                                                 c('Efficacy','Futility','Unresolved','Non-convergence'))))
     
-    pb <- txtProgressBar(min = 2, max = length(beta_list), style = 3) 
-    
-    #for (i in 2:length(beta_list)){
-    glue::glue('Overridden cores from {mc.cores} to {length(beta_list)-1}')
-    if(length(R)==1){id.seed=(1:R)+ R*(seed_set-1)}else{id.seed=R}    
-    
-    mc.cores = length(beta_list)-1
-    
+
     beta_baseline<-beta_list[1]
     beta<-list(beta_baseline,# this is the control - always
-               beta_list[i])  # this is the treatment - changes by iteration
+               beta_list[number_node+1])  # this is the treatment - changes by iteration
+
     
-    cl = parallel::makeCluster(getOption("cl.cores", mc.cores))#, type = "FORK")
-    parallel::clusterEvalQ(cl, c(library(INLA)))
-    
-  
-    
-    glm_pom_run <- parallel::mclapply(id.seed,
-                                  batss.glm.pom,
-                                  seed_set= 1,
+    glm_pom_run <- batss.glm.pom(seed_set= number_node,
                                   model=model,var=var,var.control=var.control,family=family,link=link,
                                   beta=beta,which,alternative ,R=R,N=N,
                                   interim=interim,
@@ -115,40 +104,20 @@ Wrapper<- function(
                                   fut.trial=fut.trial,fut.trial.control=fut.trial.control,
                                   RAR=RAR,RAR.control=RAR.control,
                                   H0=H0,
-                                  computation='sequential',
-                                  #mc.cores,
+                                  computation='parallel',
+                                  mc.cores=mc.cores,
                                   map_probabilities=map_probabilities,
-                                  #seed_wrapper=i,
                                   extended=extended,...)
     
-    # glm_pom_run<-batss.glm.pom( seed_set= i,
-    #                             model=model,var=var,var.control=var.control,family=family,link=link,
-    #                             beta=beta,which,alternative ,R=R,N=N,interim,prob0,
-    #                             delta.eff,delta.fut, delta.RAR,
-    #                             eff.arm,eff.arm.control,
-    #                             eff.trial,eff.trial.control,
-    #                             fut.arm,fut.arm.control,
-    #                             fut.trial,fut.trial.control,
-    #                             RAR,RAR.control,
-    #                             H0,
-    #                             computation='sequential',
-    #                             #mc.cores,
-    #                             map_probabilities,
-    #                             #seed_wrapper=i,
-    #                             extended)#), ...)
-    parallel::stopCluster(cl)   
-    
-    data[[i-1]]<-matrix(glm_pom_run$H1$estimate, ncol = 3, byrow = TRUE,
+    data[[number_node-1]]<-matrix(glm_pom_run$H1$estimate, ncol = 3, byrow = TRUE,
                       dimnames=list(paste0('Trial-',colnames(beta_list[i]),'-',1:R),
                                     c('Looks','Result','posterior Mode'))) 
     
-    summary[i-1,1]<-glm_pom_run$H1$target$global$efficacy[2] # efficacy
-    summary[i-1,2]<-glm_pom_run$H1$target$global$futility[2] # futility
-    summary[i-1,3]<-1-(glm_pom_run$H1$target$global$futility[2]+glm_pom_run$H1$target$global$efficacy[2]) # unresolved
-    summary[i-1,4]<-glm_pom_run$H1$target$global$nonconverg[2] # issues
-    setTxtProgressBar(pb, i)
+    summary[number_node-1,1]<-glm_pom_run$H1$target$global$efficacy[2] # efficacy
+    summary[number_node-1,2]<-glm_pom_run$H1$target$global$futility[2] # futility
+    summary[number_node-1,3]<-1-(glm_pom_run$H1$target$global$futility[2]+glm_pom_run$H1$target$global$efficacy[2]) # unresolved
+    summary[number_node-1,4]<-glm_pom_run$H1$target$global$nonconverg[2] # issues
     #}
-    close(pb)
     out <-list(data = data, beta_list =beta_list, summary=summary)
 }
 
@@ -156,9 +125,9 @@ Wrapper<- function(
 beta_0_select<-primOutDist_panth %>% dplyr::select(p_hypo_c, p_hypo_minus10, p_hypo_05, p_hypo_10,
                                                    p_hypo_20, p_hypo_30, p_hypo_40, p_hypo_50)
 
-Trials<-3
 
 results_wrap<-Wrapper(   
+    number_node =m,
     beta_list =beta_0_select,
     model           = y ~ treatment,
     var             = list(y = multinomial_random,
@@ -187,8 +156,8 @@ results_wrap<-Wrapper(
     delta.fut       = log(1.075), # select the analysed efficiency beta P(beta > delta.fut)
     fut.arm.control = list(b.fut = 1-0.78), # select the probability of the posterior > beta  
     delta.RAR       = 0,
-    computation     = "sequential",
-    #mc.cores        = future::availableCores()-1,
+    computation='parallel',
+    mc.cores=10,
     H0              = FALSE,
     eff.trial=efficacy.arm.fun,
     fut.trial=futility.arm.fun,
