0b2110f6fc623082f575044b48776383a67342ca
Nel Benlloch <nel.benllochg@gmail.com>
2025-06-12 12:12:53 +0100

    Update:
    Changes saved from the new file in R studio in the server

--- Rcode/Simulations_wrapper_PANTHER_parallel.R
+++ Rcode/Simulations_wrapper_PANTHER_parallel.R
@@ -47,6 +47,7 @@ library(INLA)
 
 if (length(commandArgs(trailingOnly=TRUE))!=0){
     args=commandArgs(trailingOnly=TRUE)
+    print(args)
     Trials = as.numeric(args[1])
     pres_wd = as.numeric(args[2])} else {
     setwd(paste0(rstudioapi::getSourceEditorContext()$path,"/../.."))
@@ -86,38 +87,38 @@ Wrapper<- function(
                                   dimnames=list(paste0(colnames(beta_list[,-1])),
                                                 c('Efficacy','Futility','Unresolved','Non-convergence'))))
     
-
+    
     beta_baseline<-beta_list[1]
     beta<-list(beta_baseline,# this is the control - always
                beta_list[number_node+1])  # this is the treatment - changes by iteration
-
+    
     
     glm_pom_run <- batss.glm.pom(seed_set= number_node,
-                                  model=model,var=var,var.control=var.control,family=family,link=link,
-                                  beta=beta,which,alternative ,R=R,N=N,
-                                  interim=interim,
-                                  prob0=prob0,
-                                  delta.eff=delta.eff,delta.fut=delta.fut, delta.RAR=delta.RAR,
-                                  eff.arm=eff.arm,eff.arm.control=eff.arm.control,
-                                  eff.trial=eff.trial,eff.trial.control=eff.trial.control,
-                                  fut.arm=fut.arm,fut.arm.control=fut.arm.control,
-                                  fut.trial=fut.trial,fut.trial.control=fut.trial.control,
-                                  RAR=RAR,RAR.control=RAR.control,
-                                  H0=H0,
-                                  computation='parallel',
-                                  mc.cores=mc.cores,
-                                  map_probabilities=map_probabilities,
-                                  extended=extended,...)
+                                 model=model,var=var,var.control=var.control,family=family,link=link,
+                                 beta=beta,which,alternative ,R=R,N=N,
+                                 interim=interim,
+                                 prob0=prob0,
+                                 delta.eff=delta.eff,delta.fut=delta.fut, delta.RAR=delta.RAR,
+                                 eff.arm=eff.arm,eff.arm.control=eff.arm.control,
+                                 eff.trial=eff.trial,eff.trial.control=eff.trial.control,
+                                 fut.arm=fut.arm,fut.arm.control=fut.arm.control,
+                                 fut.trial=fut.trial,fut.trial.control=fut.trial.control,
+                                 RAR=RAR,RAR.control=RAR.control,
+                                 H0=H0,
+                                 computation='parallel',
+                                 mc.cores=mc.cores,
+                                 map_probabilities=map_probabilities,
+                                 extended=extended,...)
     
     data[[number_node-1]]<-matrix(glm_pom_run$H1$estimate, ncol = 3, byrow = TRUE,
-                      dimnames=list(paste0('Trial-',colnames(beta_list[i]),'-',1:R),
-                                    c('Looks','Result','posterior Mode'))) 
+                                  dimnames=list(paste0('Trial-',colnames(beta_list[number_node]),'-',1:R),
+                                                c('Looks','Result','posterior Mode'))) 
     
     summary[number_node-1,1]<-glm_pom_run$H1$target$global$efficacy[2] # efficacy
     summary[number_node-1,2]<-glm_pom_run$H1$target$global$futility[2] # futility
     summary[number_node-1,3]<-1-(glm_pom_run$H1$target$global$futility[2]+glm_pom_run$H1$target$global$efficacy[2]) # unresolved
     summary[number_node-1,4]<-glm_pom_run$H1$target$global$nonconverg[2] # issues
-    #}
+    
     out <-list(data = data, beta_list =beta_list, summary=summary)
 }
 
@@ -125,6 +126,8 @@ Wrapper<- function(
 beta_0_select<-primOutDist_panth %>% dplyr::select(p_hypo_c, p_hypo_minus10, p_hypo_05, p_hypo_10,
                                                    p_hypo_20, p_hypo_30, p_hypo_40, p_hypo_50)
 
+
+t2=Sys.time()
 
 results_wrap<-Wrapper(   
     number_node =m,
@@ -148,7 +151,7 @@ results_wrap<-Wrapper(
     prob0           = c("UC"=1,"Simvastatin"=1),#,"Baricitinib"=1),
     N               = 504*2, # Assume the maximum cap of hypoinflammatory is reached
     interim         = list(recruited=list(m0 = 89*2    #89*3 # Trigger interim at 89 patients per arm
-                                         ,m  = 49*2)), # As per the recruitment expected Do interims at 49/ arm
+                                          ,m  = 49*2)), # As per the recruitment expected Do interims at 49/ arm
     eff.arm         = efficacy.arm.fun, # Efficiency function of posteriors
     delta.eff       = log(1.1), # Select which interims select efficiency beta P(beta > delta.fut)
     eff.arm.control = list(b.eff = 0.84), # select the probability of the posterior > beta  
@@ -156,13 +159,16 @@ results_wrap<-Wrapper(
     delta.fut       = log(1.075), # select the analysed efficiency beta P(beta > delta.fut)
     fut.arm.control = list(b.fut = 1-0.78), # select the probability of the posterior > beta  
     delta.RAR       = 0,
-    computation='parallel',
-    mc.cores=10,
+    computation     = "parallel",
+    mc.cores        = 10,
     H0              = FALSE,
     eff.trial=efficacy.arm.fun,
     fut.trial=futility.arm.fun,
     RAR = NULL,
     extended = 2)
+
+t3<-Sys.time()
+print(t3-t2)
 
 
 saveRDS(results_wrap,
