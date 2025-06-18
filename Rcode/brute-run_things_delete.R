c37a9412a0b0f0e5ab166f70e3a985a9c71ef536
Nel Benlloch <nel.benllochg@gmail.com>
2025-06-18 17:59:12 +0100

    brute changes

--- Rcode/brute-run_things_delete.R
+++ Rcode/brute-run_things_delete.R
@@ -27,9 +27,9 @@ map_probabilities = TRUE
 #RAR.control     = list("gamma"=3, "eta"=1.4,"nu"=0.1),
 #delta.RAR       = 0,
 prob0           = c("UC"=1,"Simvastatin"=1) #,"Baricitinib"=1),
-N               = 504*2 # Assume the maximum cap of hypoinflammatory is reached
+N               = 529*2 # Assume the maximum cap of hypoinflammatory is reached
 interim         = list(recruited=list(m0=89*2 #89*3 # Trigger interim at 89 patients per arm
-                                      ,m = 49*2))  # As per the recruitment expected Do interims at 49/ arm
+                                     ,m = 49*2))  # As per the recruitment expected Do interims at 49/ arm
 eff.arm         = efficacy.arm.fun # Efficiency function of posteriors
 delta.eff       = log(1.1)  # Select which interims select efficiency beta P(beta > delta.fut)
 eff.arm.control = list(b.eff = 0.84)  # select the probability of the posterior > beta  
@@ -40,14 +40,13 @@ delta.RAR=0
 computation     = "sequential"
 #mc.cores        = parallel::detectCores()-1,
 H0              = FALSE
-
-
 eff.trial=efficacy.arm.fun
 fut.trial=futility.arm.fun
 RAR = NULL
 extended = 1
 delta.RAR=0
-N = 504*2
+N = 529*2
+
 
 R=3
     
@@ -294,3 +293,75 @@ inla.pmarginal(log(futilityOR),fit_hypo$marginals.fixed$treat)>=futilityPThresh
 plot(fit_hypo$marginals.fixed$treat[,1]  ,fit_hypo$marginals.fixed$treat[,2])+ abline(v=log(1.1),col='green') + abline(v=log(1.075),col='red')
 
 plot(fit_hypo$marginals.fixed$`(Intercept`[,1]  ,fit_hypo$marginals.fixed$`(Intercept`[,2])
+
+
+
+
+setwd(paste0(rstudioapi::getSourceEditorContext()$path,"/.."))
+#  Distributions in columns, each row corresponding to an outcome value.
+primOutDist_panth<- read.csv(paste0(getwd(),"/../","excel_distributions/VFDdistributions_logodds.csv"),header=TRUE) 
+
+
+ggplot(data=primOutDist_panth, aes(x=days)) +
+    geom_line(aes(y=p_hypo_c,col='red'))+
+    geom_line(aes(y=p_hypo_80,col='blue'))+
+    #geom_line(aes(y=p_hypo_05))+
+    theme_minimal() +
+    theme(axis.text.x=element_text(size=rel(1.8)),
+          axis.text.y=element_text(size=rel(1.8)),
+          plot.title = element_text(size = rel(1.5)))+
+    scale_x_continuous(name="Days", limits=c(-1, 28), n.breaks = 29) +
+    scale_y_continuous(name="Probability")+
+    ggtitle("Probability distribution organ support")
+
+
+
+
+
+
+scenario2 = batss.glm.pom(   
+    model           = y ~ treatment,  # POM regression specification
+    var             = list(y = multinomial_random,
+                           treatment = treatalloc.fun),
+    var.control     = list(y = list(size= 1)), # Characteristics of Multinomial dist
+    family          = "pom",
+    link            = 'identity',
+    beta            = list(multinom_rand_dset1,  # this is the dist of control
+                           multinom_rand_dset2 ),# this is the dist of treatment 
+    which           = c(2),   # Select which groups are treatments
+    R               = Trials, # Number of trials to Simulate
+
+    alternative     = c("greater"), # One or two sided hypothesis
+    prob0           = c("UC"=1,"Simvastatin"=1),  ## Keep at 2 treatments for now
+    
+    N               = 504*2, # Assume the maximum cap of hypoinflammatory is reached
+    interim         = list(recruited=list(m0 = 89*2   # Trigger interim at 89 patients per arm
+                                          ,m  = 49*2  # As per the recruitment expected Do interims at 49/ arm
+    )),
+    
+    eff.arm         = efficacy.arm.fun, # Efficiency function of posteriors
+    delta.eff       = log(1.1), # Select which interims select efficiency beta P(beta > delta.fut)
+    eff.arm.control = list(b.eff = 0.84), # select the probability of the posterior > beta  
+    
+    fut.arm         = futility.arm.fun,
+    delta.fut       = log(1.075), # select the analysed efficiency beta P(beta > delta.fut)
+    fut.arm.control = list(b.fut = 1-0.78), # select the probability of the posterior > beta  
+    
+    delta.RAR       = 0,
+    eff.trial=efficacy.arm.fun,
+    fut.trial=futility.arm.fun,
+    
+    # Information on Prior
+    control.fixed = list(mean = list( treatment = 0), prec = 0.1),
+    control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE, config = TRUE), 
+    
+    # Extra specifications:
+    computation     = "parallel",
+    mc.cores        = parallel::detectCores()-1,
+    H0              = FALSE,
+    RAR = NULL,
+    map_probabilities = TRUE, # This variable will apply new maps to the days of
+                              #        day free support- estimation.
+    extended = 2)
+
+    
